112cc0b1611256093b616c46cea4eceeb7dc38e7
Alexander Brown <DrJKL0424@gmail.com>
2023-04-28 00:46:38 -0700

    cuda conf

--- webui.bat
+++ webui.bat
@@ -3,6 +3,7 @@
 if not defined PYTHON (set PYTHON=python)
 if not defined VENV_DIR (set "VENV_DIR=%~dp0%venv")
 
+set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.6,max_split_size_mb:128
 
 set ERROR_REPORTING=FALSE
 
